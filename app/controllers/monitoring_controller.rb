5888bd447fca3817b165c6e1aceb05cbddff6630
Christopher Pumihic <tophie.pumihic@gmail.com>
2018-11-02 14:35:57 +0800

    Hot Fixes: UUID creation, Monitor Mode Optimization, and Display Time Config

--- app/controllers/monitoring_controller.rb
+++ app/controllers/monitoring_controller.rb
@@ -1,6 +1,7 @@
 class MonitoringController < ApplicationController
 	layout 'monitoring'
 	def index
+		@display_time = DisplayTime.last
 		if params[:tag_uid].present?
 			@tag_uid = params[:tag_uid].to_i
 			@user = User.find_by(tag_uid: @tag_uid)
