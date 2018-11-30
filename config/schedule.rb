9adc6b54a5b479f3a6ea2a14ea85cbcf6fca8eee
Christopher Pumihic <tophie.pumihic@gmail.com>
2018-11-30 19:29:28 +0800

    Set maintenance rake

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,14 +1,9 @@
-require "./"+ File.dirname(__FILE__) + "/environment.rb"
-
-date = Settings::Configuration.first.deployment_date
 
 every :day, at: '9am' do
-	if !Student.nil?
-  	rake 'update_year_levels'
-  end
+  rake 'update_year_levels'
   rake 'expire_users'
 end
 
-every :day, at: '9am' do
-  rake 'maintenance:start' if date >= (date + 1.year)
+every 1.year, at: '9am' do
+  rake 'maintenance:start'
 end
